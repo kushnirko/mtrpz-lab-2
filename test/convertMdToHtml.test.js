4f89cb49976c9ad243eebf8f930ddb9b62c33de5
Mykola Kushnir <mykola.kushnir.825@gmail.com>
2024-04-03 21:56:00 +0300

    Write unit tests
    Cover with tests the code of Markdown validation and converting modules.

---
+++ test/convertMdToHtml.test.js
@@ -0,0 +1,103 @@
+import { describe, test, expect } from '@jest/globals';
+import convertMdToHtml from '../lib/convertMdToHtml.js';
+
+describe('Markdown to HTML converting module', () => {
+  test('paragraphs', () => {
+    const md = 'Paragraph\n1\n\nParagraph\n2';
+    const html = convertMdToHtml(md);
+    expect(html).toBe('<p>Paragraph\n1</p>\n<p>Paragraph\n2</p>');
+  });
+
+  test('preformatted text markup', () => {
+    const md = `\`\`\`
+This **text**
+\`\`\`
+contains
+\`\`\`
+3
+\`\`\`
+preformatted
+\`\`\`
+_blocks_
+\`\`\``;
+    const html = convertMdToHtml(md);
+    expect(html).toBe(`<p>
+<pre>
+This **text**
+</pre>
+contains
+<pre>
+3
+</pre>
+preformatted
+<pre>
+_blocks_
+</pre>
+</p>`);
+  });
+
+  test('bold', () => {
+    const md = '**This*text** contains **3** parts **in**bold**';
+    const html = convertMdToHtml(md);
+    expect(html).toBe(
+      '<p><b>This*text</b> contains <b>3</b> parts <b>in**bold</b></p>',
+    );
+  });
+
+  test('italic', () => {
+    const md = '_This_text_ contains _3_ parts _in__italic_';
+    const html = convertMdToHtml(md);
+    expect(html).toBe(
+      '<p><i>This_text</i> contains <i>3</i> parts <i>in__italic</i></p>',
+    );
+  });
+
+  test('monospaced', () => {
+    const md = '`This`text` contains `3` `monospaced``parts`';
+    const html = convertMdToHtml(md);
+    expect(html).toBe(
+      '<p><tt>This`text</tt> contains <tt>3</tt> <tt>monospaced``parts</tt></p>',
+    );
+  });
+
+  test('different length of inline elements', () => {
+    const md = '**I** _do_ `not` _like_ **bugs!**';
+    const html = convertMdToHtml(md);
+    expect(html).toBe('<p><b>I</b> <i>do</i> <tt>not</tt> <i>like</i> <b>bugs!</b></p>');
+  });
+
+  test('text with punctuations', () => {
+    const md = 'Are "`there`" (_too_) many **punctuations**, in this `text`?';
+    const html = convertMdToHtml(md);
+    expect(html).toBe(
+      '<p>Are "<tt>there</tt>" (<i>too</i>) many <b>punctuations</b>, in this <tt>text</tt>?</p>',
+    );
+  });
+
+  test('cyrillic text', () => {
+    const md = 'Ходить **гарбуз**\n`по` городу';
+    const html = convertMdToHtml(md);
+    expect(html).toBe('<p>Ходить <b>гарбуз</b>\n<tt>по</tt> городу</p>');
+  });
+
+  test('comprehensive test', () => {
+    const md = `\`*\` _This_text_ contains
+paragraphs,
+
+\`\`\`
+**pre_formatted** block
+\`\`\`
+and **different types** of
+
+\`inline\` elements.`;
+    const html = convertMdToHtml(md);
+    expect(html).toBe(`<p><tt>*</tt> <i>This_text</i> contains
+paragraphs,</p>
+<p>
+<pre>
+**pre_formatted** block
+</pre>
+and <b>different types</b> of</p>
+<p><tt>inline</tt> elements.</p>`);
+  });
+});
---
+++ test/validateMd.test.js
@@ -0,0 +1,65 @@
+import { describe, test, expect } from '@jest/globals';
+import validateMd from '../lib/validateMd.js';
+
+describe('Markdown validation module', () => {
+  test('plain text', () => {
+    const md = 'This text\ncontains\nno markup';
+    const err = validateMd(md);
+    expect(err).toBeNull();
+  });
+
+  // TODO: change to 'too many blank lines'
+  test('too many blank lines between paragraphs', () => {
+    const md = 'Paragraph 1\n\n\nParagraph 2';
+    const err = validateMd(md);
+    expect(err).toBeInstanceOf(Error);
+    expect(err.message).toBe('invalid markdown (too many spaces between paragraphs)');
+  });
+
+  test('unpaired markup of preformatted text', () => {
+    const md = 'This text should\n```\ncause an error';
+    const err = validateMd(md);
+    expect(err).toBeInstanceOf(Error);
+    expect(err.message).toBe('invalid markdown (unpaired  markup of preformatted text)');
+  });
+
+  test('valid markup of preformatted text', () => {
+    const md = '```\nThis text\n```\nshould not cause an\n```\nerror\n```';
+    const err = validateMd(md);
+    expect(err).toBeNull();
+  });
+
+  test('unpaired markup of inline element', () => {
+    const md = 'This text should **cause an error';
+    const err = validateMd(md);
+    expect(err).toBeInstanceOf(Error);
+    expect(err.message).toBe('invalid markdown (unpaired markup of inline element)');
+  });
+
+  test('nesting markup', () => {
+    const md = 'This `text **should cause**` an error';
+    const err = validateMd(md);
+    expect(err).toBeInstanceOf(Error);
+    expect(err.message).toBe('invalid markdown (nesting markup)');
+  });
+
+  test('valid markup of inline elements', () => {
+    const md = '`This_text` should **not** cause**an _error_';
+    const err = validateMd(md);
+    expect(err).toBeNull();
+  });
+
+  test('comprehensive test', () => {
+    const md = `\`*\` _This_text_ contains
+paragraphs,
+
+\`\`\`
+**pre\`formatted** block
+\`\`\`
+and **different types** of
+
+\`inline\` elements.`;
+    const err = validateMd(md);
+    expect(err).toBeNull();
+  });
+});
